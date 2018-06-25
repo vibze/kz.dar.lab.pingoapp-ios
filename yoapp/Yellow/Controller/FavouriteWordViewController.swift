386c3e08417e45d920ba16411db8305db34f06a3
Kamila <kusainovaka@gmail.com>
2018-06-25 10:33:41 +0600

    settings

--- yoapp/Yellow/Controller/FavouriteWordViewController.swift
+++ yoapp/Yellow/Controller/FavouriteWordViewController.swift
@@ -12,6 +12,7 @@ class FavouriteWordViewController: UITableViewController {
     
     let headerView = SettingHeaderView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 80))
     var favourCell = "FavouriteCell"
+    var array = ["Привет, как дела?","Привет","Что делаешь?","Привет, как дела?","Привет","Что делаешь?"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,23 +27,38 @@ class FavouriteWordViewController: UITableViewController {
         headerView.titleName(title: "Избранные фразы")
         tableView.tableHeaderView = headerView
         headerView.backButton.addTarget(self, action: #selector(bactToVC), for: .touchUpInside)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: favourCell)
+        tableView.register(SettingViewCell.self, forCellReuseIdentifier: favourCell)
     }
 }
 
 extension FavouriteWordViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return array.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: favourCell, for: indexPath) as UITableViewCell
-        cell.backgroundColor = .myYellow
-        cell.textLabel?.text = "Привет! Как дела?"
-        cell.textLabel?.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        let cell = tableView.dequeueReusableCell(withIdentifier: favourCell, for: indexPath) as! SettingViewCell
+//        cell.backgroundColor = .myYellow
+//        cell.textLabel?.text = array[indexPath.row]
+//        cell.textLabel?.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        cell.textName(text: array[indexPath.row])
         
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteButton = UITableViewRowAction(style: .default, title: "Удалить") { (action, indexPath) in
+            self.tableView.dataSource?.tableView!(self.tableView, commit: .delete, forRowAt: indexPath)
+            return
+        }
+        return [deleteButton]
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.array.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
