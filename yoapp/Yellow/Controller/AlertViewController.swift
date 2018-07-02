2d21baefb276d67eb02ec68ae5ce363f1d2b649c
Kamila <kusainovaka@gmail.com>
2018-07-02 15:43:33 +0600

    Alert VC changed

--- yoapp/Yellow/Controller/AlertViewController.swift
+++ yoapp/Yellow/Controller/AlertViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class AlertViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let alertView = AlertView()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        modalPresentationStyle = .overCurrentContext
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.55)
+        view.addSubview(alertView)
+        configView()
+    }
+    
+    func configView(){
+        alertView.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(190)
+            $0.left.equalToSuperview().offset(24)
+            $0.right.equalToSuperview().offset(-24)
+            $0.bottom.equalToSuperview().offset(-250)
+        }
+        
+        alertView.okButton.addTarget(self, action: #selector(okAction), for: .touchUpInside)
+    }
+    
+    func errorView(errorType: String, image: UIImage){
+        alertView.errorTypeLabel.text = errorType
+        alertView.imgView.image = image
+    }
+    
+    @objc func okAction(){
+    dismiss(animated: true, completion: nil)
+    }
 }
--- yoapp/Yellow/View/AlertView.swift
+++ yoapp/Yellow/View/AlertView.swift
@@ -6,4 +6,63 @@
 //  Copyright © 2018 Kurmanbay Ayan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AlertView: UIView {
+    
+    var okButton: UIButton =  {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        button.isEnabled = true
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = #colorLiteral(red: 0.1568627451, green: 0.4784313725, blue: 1, alpha: 1)
+        button.layer.borderWidth = 5
+        button.setTitle("OK", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0.9803921569, green: 0.3529411765, blue: 0.3411764706, alpha: 1), for: .normal)
+        return button
+    }()
+    
+    let imgView = UIImageView()
+    let errorTypeLabel = UILabel.basic(textColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), fontSize: 18, fontType: .mySemiBold)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor(hexString: "3C97E8")
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 5
+        self.layer.borderColor = UIColor(hexString: "3070CD").cgColor
+        
+        self.addSubview(imgView)
+        self.addSubview(errorTypeLabel)
+        self.addSubview(okButton)
+        configView()
+    }
+    
+    func configView(){
+        imgView.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(30)
+            $0.left.equalToSuperview().offset(116)
+            $0.right.equalToSuperview().offset(-116)
+            $0.height.equalTo(78)
+        }
+        
+        errorTypeLabel.snp.makeConstraints{
+            $0.top.equalTo(imgView.snp.bottom).offset(10)
+//            $0.centerX.equalToSuperview()
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+        }
+        
+        okButton.snp.makeConstraints{
+            $0.bottom.equalToSuperview().offset(-12)
+            $0.left.equalToSuperview().offset(64)
+            $0.right.equalToSuperview().offset(-64)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
