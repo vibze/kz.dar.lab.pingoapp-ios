9be0c78e7333c487e55b2b48616c0bcddd80877a
Ayan <ayan.kurmanbai@gmail.com>
2018-07-11 15:25:02 +0600

    Added insets to text view

--- yoapp/Green/Compose Page/Controllers/ComposeViewController.swift
+++ yoapp/Green/Compose Page/Controllers/ComposeViewController.swift
@@ -16,21 +16,9 @@ class ComposeViewController: UIViewController {
     let composeButton = ActionButton(title: "Отправить сообщение", type: .write)
     let profileImageView = ImageView(radius: 90 / 2)
     let alertView = PushAlert()
-   
+    let messageTextView = MessageTextView()
+
     var contact: Contact?
-    
-    let messageTextView: UITextView = {
-        let textView = UITextView()
-        textView.layer.masksToBounds = true
-        textView.layer.cornerRadius = 10
-        textView.isUserInteractionEnabled = true
-        textView.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6274509804, blue: 0.4745098039, alpha: 1).cgColor
-        textView.layer.borderWidth = 5
-        textView.font = UIFont(name: "Avenir Next", size: 18)
-        textView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        textView.layer.sublayerTransform = CATransform3DMakeTranslation(8, 0, 0)
-        return textView
-    }()
     
     @objc func backButtonPressed() {
         self.dismiss(animated: true, completion: nil)
