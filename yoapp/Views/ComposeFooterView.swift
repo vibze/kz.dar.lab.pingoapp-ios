eb2e0aebcf78ccc378bae50e6db3ffcd9402eea3
Kamila <kusainovaka@gmail.com>
2018-07-17 02:31:41 +0600

    Compose view fix

--- yoapp/Views/ComposeFooterView.swift
+++ yoapp/Views/ComposeFooterView.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2018 Kurmanbay Ayan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ComposeFooterView: UIView, UITextViewDelegate {
+    
+    var messageText: UITextView = {
+        let textView = UITextView()
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = 10
+        textView.isUserInteractionEnabled = true
+        textView.layer.borderColor = #colorLiteral(red: 0.3529411765, green: 0.6274509804, blue: 0.4745098039, alpha: 1).cgColor
+        textView.layer.borderWidth = 5
+        textView.font = UIFont(name: "Avenir Next", size: 18)
+        textView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        textView.layer.sublayerTransform = CATransform3DMakeTranslation(8, 0, 0)
+        textView.contentInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 10)
+        return textView
+    }()
+    
+    let composeButton = ActionButton(title: "Отправить сообщение", type: .write)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(messageText)
+        self.addSubview(composeButton)
+        configView()
+    }
+    
+    func configView(){
+        messageText.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.left.equalToSuperview().offset(32)
+            $0.width.equalToSuperview().inset(32)
+            $0.height.equalTo(100)
+        }
+        
+        composeButton.snp.makeConstraints {
+            $0.top.equalTo(messageText.snp.bottom).offset(24)
+            $0.left.equalToSuperview().offset(32)
+            $0.width.equalToSuperview().inset(32)
+            $0.height.equalTo(50)
+        }
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let flexible = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let doneBTN = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.hideKeyboard))
+        
+        toolBar.setItems([flexible, doneBTN], animated: false)
+        messageText.inputAccessoryView = toolBar
+    }
+    
+    @objc func hideKeyboard(){
+        self.endEditing(true)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- yoapp/Views/ComposeHeaderView.swift
+++ yoapp/Views/ComposeHeaderView.swift
@@ -6,4 +6,64 @@
 //  Copyright © 2018 Kurmanbay Ayan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ComposeHeaderView: UIView {
+    
+    var backgorunCircle: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.3568627451, green: 0.631372549, blue: 0.4784313725, alpha: 1)
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 55
+        return view
+    }()
+    
+    let profileImg = ImageView(radius: 50)
+    let profileNameLabel = UILabel.basic(textColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), fontSize: 20, fontType: FontType.myRegular)
+    let phoneNumberLabel = UILabel.basic(textColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), fontSize: 20, fontType: FontType.myRegular)
+ 
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews(){
+        self.addSubview(backgorunCircle)
+        self.addSubview(profileImg)
+        self.addSubview(profileNameLabel)
+        self.addSubview(phoneNumberLabel)
+        
+        backgorunCircle.snp.makeConstraints{(make) in
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(110)
+        }
+        
+        profileImg.snp.makeConstraints{(make) in
+            make.top.equalTo(backgorunCircle.snp.top).offset(5)
+            make.size.equalTo(100)
+            make.centerX.equalToSuperview()
+        }
+        
+        profileNameLabel.snp.makeConstraints{(make) in
+            make.top.equalTo(profileImg.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+        }
+        
+        phoneNumberLabel.snp.makeConstraints{
+            $0.top.equalTo(profileNameLabel.snp.bottom).offset(3)
+            $0.left.right.equalTo(profileNameLabel)
+        }
+    }
+    
+    func viewData(image: String, phoneNumber: String, profileName: String) {
+        profileNameLabel.text = profileName
+        phoneNumberLabel.text = "+\(phoneNumber)"
+        profileImg.setContactImage(url: image)
+    }
+}
